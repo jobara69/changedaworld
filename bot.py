cd76fe579b418caaf24691c0252d95775a05edde
Jobara <kromaxjm@gmail.com>
2019-10-29 14:44:12 -0300

    'margarina'

--- bot.py
+++ bot.py
@@ -6,11 +6,6 @@ client = commands.Bot(command_prefix = '.')
 
 @client.event
 async def on_ready():
-    await client.change_presence(activity = discord.Game ('me mata, por favor'))
     print ('Bot is ready')
-
-@client.command()
-async def ping(ctx):
-    await ctx.send('Pong!')
 
 client.run(os.environ['DISCORD_TOKEN'])
